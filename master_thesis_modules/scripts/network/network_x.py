97460fd38d22c5222e4ec97895c0d981ee94e945
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2024-12-02 14:59:12 +0900

    network manager

---
+++ master_thesis_modules/scripts/network/network_x.py
@@ -0,0 +1,212 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class GraphManager():
+    def __init__(self):
+        # define network
+        self.G = nx.Graph()
+        
+        # node定義
+        self.node_dict={
+            1000:{
+                "score":np.nan,
+                "status":"active",
+                "description":"最も行動が危険な人物の選定"
+                },
+            2000:{
+                "score":np.nan,
+                "status":"active",
+                "description":"転倒事故防止"
+                },
+            2001:{
+                "score":np.nan,
+                "status":"active",
+                "description":"スタッフの緊張緩和"
+                },
+            2002:{
+                "score":np.nan,
+                "status":"active",
+                "description":"患者の主体性担保"
+                },
+            3000:{
+                "score":np.nan,
+                "status":"active",
+                "description":"本人の属性"
+                },
+            3001:{
+                "score":np.nan,
+                "status":"active",
+                "description":"本人の動作"
+                },
+            3002:{
+                "score":np.nan,
+                "status":"active",
+                "description":"本人の様子"
+                },
+            3003:{
+                "score":np.nan,
+                "status":"active",
+                "description":"本人の動機"
+                },
+            3004:{
+                "score":np.nan,
+                "status":"active",
+                "description":"時間的文脈"
+                },
+            3005:{
+                "score":np.nan,
+                "status":"active",
+                "description":"空間的文脈"
+                },
+            4050:{
+                "score":np.nan,
+                "status":"inactive",
+                "description":"本人の居場所"
+                },
+            4051:{
+                "score":np.nan,
+                "status":"inactive",
+                "description":"周囲の物体"
+                },
+            4052:{
+                "score":np.nan,
+                "status":"inactive",
+                "description":"周囲の人物"
+                },
+            5510:{
+                "score":np.nan,
+                "status":"inactive",
+                "description":"経管栄養・点滴の存在"
+                },
+            5511:{
+                "score":np.nan,
+                "status":"inactive",
+                "description":"車椅子の存在"
+                },
+            5512:{
+                "score":np.nan,
+                "status":"inactive",
+                "description":"手すりの不在"
+                },
+            5520:{
+                "score":np.nan,
+                "status":"inactive",
+                "description":"看護師の不在"
+                },
+            5521:{
+                "score":np.nan,
+                "status":"inactive",
+                "description":"介護士の不在"
+                },
+            5522:{
+                "score":np.nan,
+                "status":"inactive",
+                "description":"面会者の存在"
+                },
+        }
+        self.G.add_nodes_from(self.node_dict.keys())
+
+        # 重み定義
+        self.weight_dict={
+            1000:{
+                2000:1/3,
+                2001:1/3,
+                2002:1/3,
+            },
+            2000:{
+                3000:1/6,
+                3001:1/6,
+                3002:1/6,
+                3003:1/6,
+                3004:1/6,
+                3005:1/6,
+            },
+            # 2001:{},
+            # 2002:{},
+            # 3000:{},
+            # 3001:{},
+            # 3002:{},
+            # 3003:{},
+            # 3004:{},
+            3005:{
+                4050:1/3,
+                4051:1/3,
+                4052:1/3,
+            },
+            # 4050:{},
+            4051:{
+                5510:1/3,
+                5511:1/3,
+                5512:1/3,
+            },
+            4052:{
+                5520:1/3,
+                5521:1/3,
+                5522:1/3,
+            },
+        }
+
+        # edge定義
+        for lv in range(5):
+            for node_code_from in [n for n in list(self.node_dict.keys()) if str(n)[0]==str(lv)]:
+                if node_code_from=="description":
+                    continue
+                node_codes_to=[k for k in [j for j in list(self.node_dict.keys()) if str(j)[0]==str(lv+1)] if str(node_code_from)[2:] == str(k)[1:3]]
+                self.G.add_edges_from([(node_code_from,node_code_to,{"weight":self.weight_dict[node_code_from][node_code_to]}) for node_code_to in node_codes_to])
+
+        # 位置追記
+        self.pos={}
+        previous_layer=0
+        for node_code in self.G.nodes():
+            if previous_layer!=int(str(node_code)[0]):
+                y=0
+            else:
+                y-=1
+            self.pos[node_code]=(int(str(node_code)[0]),y)
+            previous_layer=int(str(node_code)[0])
+
+    def update_score(self,new_score_dict):
+        for node_code,score in new_score_dict.items():
+            self.node_dict[node_code]["score"]=score
+
+    def update_weight(self,new_weight_dict):
+        for node_from in new_weight_dict.keys():
+            for node_to in new_weight_dict[node_from].keys():
+                self.weight_dict[node_from][node_to]=new_weight_dict[node_from][node_to]
+    
+    def update_lower_layer_status(self,new_status="active"):
+        for node in self.G.nodes():
+            if int(str(node)[0])>=4:
+                self.node_dict[node]["status"]=new_status
+        self.colorize(default=False)
+        
+    def colorize(self,default=True):
+        # 色追記
+        self.nodecolor=[]
+        if default:
+            for node_code in self.G.nodes():
+                self.nodecolor.append((1,0,0) if self.node_dict[node_code]["status"]=="active" else "gray")
+        else:
+            for node_code in self.G.nodes():
+                node=self.node_dict[node_code]
+                if np.isnan(node["score"]):
+                    self.nodecolor.append((1,1,1) if node["status"]=="active" else "gray")
+                else:
+                    self.nodecolor.append((node["score"],0,0) if node["status"]=="active" else "gray")
+            pass
+        
+    def visualize(self):
+        weights = nx.get_edge_attributes(self.G, 'weight').values()
+        nx.draw(self.G, self.pos, node_color=self.nodecolor, with_labels=True, edge_color = weights, edge_cmap=plt.cm.RdBu_r)
+        plt.show()
+
+    def main(self):
+        self.colorize(default=False)
+        self.visualize()
+        self.update_lower_layer_status()
+        self.visualize()
+
+if __name__=="__main__":
+    cls=GraphManager()
+    cls.main()
--- master_thesis_modules/scripts/throttling/lab_throttling.py
+++ master_thesis_modules/scripts/throttling/lab_throttling.py
@@ -1,16 +1,153 @@
 import numpy as np
+import pandas as pd
+from icecream import ic # pip install icecream
+
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 class ThrottlingLab():
     def __init__(self):
+        t=np.arange(0,10,0.1)
+        self.data=pd.DataFrame(t,columns=["timestamp"])
+        self.weights_dict={}
+        self.threshold_dict={
+            "total_score_thre":0.5,
+        }
+        self.status_color={"active":"red","inactive":"blue"}
+
+
+    def define_observed_values(self):
+        def add_risk_curve(feature_points,col_name):
+            x_values = feature_points[:, 0]
+            y_values = feature_points[:, 1]
+            interpolated_values = np.interp(self.data["timestamp"], x_values, y_values)
+            self.data[col_name]=interpolated_values
+        fp_xA=np.array([
+            [0,0.1],
+            [1,0.1],
+            [2,0.9],
+            [4,0.9],
+            [6,0.7],
+            [8,0.9],
+            [9,0.1],
+            [10,0.1]
+        ])
+        fp_xB1=np.array([
+            [0,0.9],
+            [4,0.9],
+            [7,0.1],
+            [10,0.1],
+        ])
+        fp_xB2=np.array([
+            [0,0.7],
+            [10,0.7],
+        ])
+        add_risk_curve(fp_xA,"xA") # イベントカメラの値
+        add_risk_curve(fp_xB1,"xB1") # スタッフとの距離（遠さ）
+        add_risk_curve(fp_xB2,"xB2") # 点滴との距離（近さ）
         pass
+
+    def define_weights_dict(self):
+        self.weights_dict["lv_03"]={
+            "wA":{"value":0.5,"level":"lv_03","description":"HOW"},
+            "wB":{"value":0.5,"level":"lv_03","description":"WHERE"},
+        }
+        self.weights_dict["lv_04"]={
+            "wA":{},
+            "wB":{
+                "wB1":{"value":0.5,"level":"lv_04","description":"スタッフとの距離"},
+                "wB2":{"value":0.5,"level":"lv_04","description":"点滴との距離"},
+            }
+        }
+        pass
+
+    def get_score(self,method="sum"):
+        if method=="sum":
+            self.score_A=self.weights_dict["lv_03"]["wA"]["value"]*self.data["xA"]
+            self.score_B=self.weights_dict["lv_03"]["wB"]["value"]*(self.weights_dict["lv_04"]["wB"]["wB1"]["value"]*self.data["xB1"]+self.weights_dict["lv_04"]["wB"]["wB2"]["value"]*self.data["xB2"])
+            self.data["score"]=self.score_A+self.score_B
+        elif method=="throttling":
+            self.data["score_A"]=np.nan
+            self.data["score_B"]=np.nan
+            self.data["score"]=np.nan
+            self.data["throttling_status"]=""
+            score_lv_04_B=np.nan
+            throttling_status="inactive"
+            for i,row in self.data.iterrows():
+                # lv_04の計算
+                self.data.loc[i,"throttling_status"]=throttling_status
+                ic(throttling_status)
+                if i==0:
+                    score_lv_04_B=self.weights_dict["lv_04"]["wB"]["wB1"]["value"]*self.data.loc[i,"xB1"]+self.weights_dict["lv_04"]["wB"]["wB2"]["value"]*self.data.loc[i,"xB2"]
+                elif throttling_status=="active":
+                    score_lv_04_B=self.weights_dict["lv_04"]["wB"]["wB1"]["value"]*self.data.loc[i,"xB1"]+self.weights_dict["lv_04"]["wB"]["wB2"]["value"]*self.data.loc[i,"xB2"]
+                elif throttling_status=="inactive":
+                    pass
+                
+                # scoreの計算
+                self.data.loc[i,"score_A"]=self.weights_dict["lv_03"]["wA"]["value"]*self.data.loc[i,"xA"]
+                self.data.loc[i,"score_B"]=self.weights_dict["lv_03"]["wB"]["value"]*score_lv_04_B
+                self.data.loc[i,"score"]=self.data.loc[i,"score_A"]+self.data.loc[i,"score_B"]
+
+                # statusの判定
+                if self.data.loc[i,"score"]>self.threshold_dict["total_score_thre"]:
+                    throttling_status="active"
+                else:
+                    throttling_status="inactive"
+            pass
+        else:
+            raise NotImplementedError
+        pass
+
+    def visualize(self):
+        gs=GridSpec(nrows=3,ncols=1)
+        plt.subplot(gs[0])
+        plt.plot(self.data["timestamp"],self.data["xA"],label="xA (move)")
+        plt.plot(self.data["timestamp"],self.data["xB1"],label="xB1 (staff distance)")
+        plt.plot(self.data["timestamp"],self.data["xB2"],label="xB2 (infusion distance)")
+        plt.xlim([self.data["timestamp"].min()-1,self.data["timestamp"].max()+1])
+        plt.xlabel("Time [s]")
+        plt.ylabel("Risk value")
+        plt.legend()
+        plt.grid()
+
+        plt.subplot(gs[1])
+        # plt.plot(self.data["timestamp"],self.score_A,"--",label="score_A")
+        # plt.plot(self.data["timestamp"],self.score_B,"--",label="score_B")
+        plt.plot(self.data["timestamp"],self.data["score"],"--",label="score")
+        plt.xlim([self.data["timestamp"].min()-1,self.data["timestamp"].max()+1])
+        plt.xlabel("Time [s]")
+        plt.ylabel("Risk value")
+        plt.legend()
+        plt.grid()
+
+        plt.subplot(gs[2])
+        for i,row in self.data.iterrows():
+            # ic(self.data.loc[i,"throttling_status"])
+            plt.barh(y=0,left=self.data.loc[i,"timestamp"],width=0.1,color=self.status_color[self.data.loc[i,"throttling_status"]])
+        plt.xlim([self.data["timestamp"].min()-1,self.data["timestamp"].max()+1])
+        plt.xlabel("Time [s]")
+        plt.ylabel("Throttling status")
+        plt.legend()
+        plt.grid()
+        plt.show()
 
     def main(self):
-        # define truth values
+        # define observed values
+        self.define_observed_values()
+        ic(self.data)
+
+        # define weight
+        self.define_weights_dict()
+        ic(self.weights_dict)
 
         # simulation
+        self.get_score(method="throttling")
 
         # visualize
-        pass
+        self.visualize()
 
 if __name__=="__main__":
+    cls=ThrottlingLab()
+    cls.main()
     pass
