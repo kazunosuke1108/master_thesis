48e39638e97d4c59ec5ca0abf9e26762b8d60fa3
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2024-12-07 12:42:07 +0900

    add angle

--- master_thesis_modules/scripts/pseudo_data/pseudo_data_generator_ABC.py
+++ master_thesis_modules/scripts/pseudo_data/pseudo_data_generator_ABC.py
@@ -5,7 +5,7 @@ from icecream import ic
 class PseudoDataGenerator_ABC():
     def __init__(self):
         super().__init__()
-        self.t=np.arange(0,10,0.25)
+        self.t=np.arange(0,8.001,0.25)
         data=pd.DataFrame(self.t,columns=["timestamp"])
         columns=[1000,2000,2001,2002,3000,3001,3002,3003,3004,3005,4050,4051,4052,5510,5511,5512,5520,5521,5522]
         columns=columns+["active"]
@@ -157,7 +157,7 @@ class PseudoDataGenerator_ABC():
                 return traj
             elif name==5520:
                 traj=np.array([
-                    [0,5,5],
+                    [0,5.1,5.1],
                     [2,5,5],
                     [4,3,3],
                     [6,2.2,2.2],
@@ -216,6 +216,15 @@ class PseudoDataGenerator_ABC():
                 return 1
             else:
                 return theta/90
+            
+        def get_theta(object_vel,relative_pos):
+            temp=np.dot(object_vel,relative_pos)/(np.linalg.norm(object_vel)*np.linalg.norm(relative_pos))
+            if temp>1:
+                temp=1
+            elif temp<-1:
+                temp=-1
+            theta=np.rad2deg(np.arccos(temp))
+            return theta
         for patient_name in patients_position_dict.keys():
             for surrounding_name in surroundings_position_dict.keys():
                 # print(patients_position_dict[patient_name].loc[:,"x"]-surroundings_position_dict[surrounding_name].loc[:,"x"])
@@ -227,10 +236,15 @@ class PseudoDataGenerator_ABC():
                     self.data_dict[patient_name][surrounding_name]=1-np.array(list(map(membership_distance,distance)))
                 else:
                     self.data_dict[patient_name][surrounding_name]=np.array(list(map(membership_distance,distance)))
-                    ic(self.data_dict[patient_name][surrounding_name])
+                    # ic(self.data_dict[patient_name][surrounding_name])
                 if surrounding_name==5520: # 看護師の距離を算出済みの場合
-                    # 間隔
-                    pass
+                    # 速度
+                    object_vel=surroundings_position_dict[surrounding_name][["x","y"]].diff().values
+                    relative_pos=patients_position_dict[patient_name][["x","y"]].values-surroundings_position_dict[surrounding_name][["x","y"]].values
+                    theta=np.array(list(map(get_theta,object_vel,relative_pos)))
+                    self.data_dict[patient_name][5521]=np.array(list(map(membership_view,theta)))
+                    self.data_dict[patient_name][5521].fillna(value=0,inplace=True)
+                    ic(self.data_dict[patient_name])
         pass
 
         
