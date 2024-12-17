f2b29e5a95903ea797b04299e8db02c04f2c3de6
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2024-12-18 00:04:40 +0900

    EWM for t-direction: 患者間の差が小さくなって分かりにくい．ノイズを与えるときのコーディングにもミスが残る

--- master_thesis_modules/scripts/entropy/entropy_weight_generator.py
+++ master_thesis_modules/scripts/entropy/entropy_weight_generator.py
@@ -14,14 +14,28 @@ class EntropyWeightGenerator():
         # 正規化
         score_df=score_df/score_df.sum()
         entropy=-1/np.log(len(score_df.index))*(score_df*np.log(score_df)).sum()
+        entropy[np.isnan(entropy)]=1e-10
         degree_of_diversification=1-entropy
         weight=degree_of_diversification/degree_of_diversification.sum()
         # ic(score_df)
+        # ic(weight.to_dict())
+        return weight.to_dict()
+    
+    def get_entropy_weight_t(self,score_df=pd.DataFrame(data=np.random.random((3,6)),columns=[3000,3001,3002,3003,3004,3005])):
+        # 正規化
+        score_df=score_df/score_df.sum()
+        # 列ごとにエントロピーを計算していく
+        ic(score_df.index)
+        ic(-1/np.log(len(score_df.index))*(score_df*np.log(score_df)))
+        entropy=-1/np.log(len(score_df.index))*(score_df*np.log(score_df)).sum()
+        degree_of_diversification=1-entropy
+        weight=degree_of_diversification/degree_of_diversification.sum()
+        ic(score_df)
         # ic(weight.to_dict())
         return weight.to_dict()
 
 
 if __name__=="__main__":
     cls=EntropyWeightGenerator()
-    cls.get_entropy_weight()
+    cls.get_entropy_weight_t()
     pass
