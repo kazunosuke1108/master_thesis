310832463e991a3944adbddc14d5de9e6294467c
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2025-01-23 13:52:17 +0900

    2023a

--- master_thesis_modules/scripts/master_comprehensive_analysis.py
+++ master_thesis_modules/scripts/master_comprehensive_analysis.py
@@ -137,8 +137,8 @@ class ComprehensiveAnalysis():
         }
         standups=[
             # ["A"],
-            ["B"],
             # ["B"],
+            ["C"],
             # ["A","B"],
             # ["A","B","C"],
             ]
@@ -205,7 +205,7 @@ class ComprehensiveAnalysis():
         cls_master.save_session()
 
     def comprehensive_analysis_main(self):
-        simulation_name="20250113SimulationPositionB"
+        simulation_name="20250113SimulationPositionC2"
         strage="NASK"
         runtype="simulation"
         condition_dicts=self.generate_condition_dicts(simulation_name)
